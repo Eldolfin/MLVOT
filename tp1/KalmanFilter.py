b0ecf730595daeaf5ab5d182495cb42a8523d2b9
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-12-14 18:32:08 +0100

    cleanup + format tp 1, move files around

--- tp1/KalmanFilter.py
+++ tp1/KalmanFilter.py
@@ -1,44 +1,43 @@
 import numpy as np
 
+
 class KalmanFilter:
-    def __init__(self, dt: float, u_x: float, u_y: float, std_acc: float, x_dt_meas: float, y_dt_meas: float):
+    def __init__(
+        self,
+        dt: float,
+        u_x: float,
+        u_y: float,
+        std_acc: float,
+        x_dt_meas: float,
+        y_dt_meas: float,
+    ):
         self.u = (u_x, u_y)
         self.alexis = np.zeros(4)
-        self.A = np.asarray([
-            [1, 0, dt, 0],
-            [0, 1, 0, dt],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
-        ])
-        dt_square = dt ** 2
-        self.B = np.asarray([
-            [0.5 * dt_square, 0],
-            [0, 0.5 * dt_square],
-            [dt, 0],
-            [0, dt]
-        ])
-        self.H = np.asarray([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0]
-        ])
-        dt_3 = dt ** 3 / 2.
-        dt_4 = dt ** 4 / 4.
-        self.Q = np.asarray([
-            [dt_4, 0, dt_3, 0],
-            [0, dt_4, 0, dt_3],
-            [dt_3, 0, dt_square, 0],
-            [0, dt_3, 0, dt_square]
-        ]) * std_acc ** 2
-        self.R = np.asarray([
-            [x_dt_meas ** 2, 0],
-            [0, y_dt_meas ** 2]
-        ])
+        self.A = np.asarray([[1, 0, dt, 0], [0, 1, 0, dt], [0, 0, 1, 0], [0, 0, 0, 1]])
+        dt_square = dt**2
+        self.B = np.asarray(
+            [[0.5 * dt_square, 0], [0, 0.5 * dt_square], [dt, 0], [0, dt]]
+        )
+        self.H = np.asarray([[1, 0, 0, 0], [0, 1, 0, 0]])
+        dt_3 = dt**3 / 2.0
+        dt_4 = dt**4 / 4.0
+        self.Q = (
+            np.asarray(
+                [
+                    [dt_4, 0, dt_3, 0],
+                    [0, dt_4, 0, dt_3],
+                    [dt_3, 0, dt_square, 0],
+                    [0, dt_3, 0, dt_square],
+                ]
+            )
+            * std_acc**2
+        )
+        self.R = np.asarray([[x_dt_meas**2, 0], [0, y_dt_meas**2]])
         self.P = np.identity(4)
 
     def predict(self):
         self.alexis = self.A @ self.alexis + self.B @ self.u
         self.P = self.A @ self.P * self.A.T + self.Q
-
 
     def update(self, cur_pos: np.array):
         S_k = self.H @ self.P @ self.H.T + self.R
@@ -46,4 +45,3 @@ class KalmanFilter:
         self.alexis = self.alexis + K_k @ (cur_pos.T - self.H @ self.alexis)[0]
         self.P = (np.identity(4) - K_k @ self.H) @ self.P
         return self.alexis
-
--- tp1/main.py
+++ tp1/main.py
@@ -1,18 +1,20 @@
 import Detector
 from KalmanFilter import KalmanFilter
 import cv2 as cv
-import numpy as np
+
 
 def main():
-    cap = cv.VideoCapture('./video/randomball.avi')
+    cap = cv.VideoCapture("./video/randomball.avi")
 
     width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-    fourcc = cv.VideoWriter_fourcc(*'XVID')
-    out = cv.VideoWriter('output.avi', fourcc, 20.0, (width, height))
+    fourcc = cv.VideoWriter_fourcc(*"XVID")
+    out = cv.VideoWriter("output.avi", fourcc, 60.0, (width, height))
 
-    kalman_filter = KalmanFilter(dt=0.1, u_x=1, u_y=1, std_acc=1, x_dt_meas=0.1, y_dt_meas=0.1)
+    kalman_filter = KalmanFilter(
+        dt=0.1, u_x=1, u_y=1, std_acc=1, x_dt_meas=0.1, y_dt_meas=0.1
+    )
 
     trajectory = []
 
@@ -28,19 +30,31 @@ def main():
             kalman_filter.predict()
             pred = kalman_filter.update(center)
             # __import__("ipdb").set_trace()
-            cv.rectangle(frame, (int(center[0]) - 15, int(center[1]) - 15), (int(center[0]) + 15, int(center[1]) + 15), (255, 0, 0), 2)
-            cv.rectangle(frame, (int(pred[0]) - 15, int(pred[1]) - 15), (int(pred[0]) + 15, int(pred[1]) + 15), (0, 0, 255), 2)
+            cv.rectangle(
+                frame,
+                (int(center[0]) - 15, int(center[1]) - 15),
+                (int(center[0]) + 15, int(center[1]) + 15),
+                (255, 0, 0),
+                2,
+            )
+            cv.rectangle(
+                frame,
+                (int(pred[0]) - 15, int(pred[1]) - 15),
+                (int(pred[0]) + 15, int(pred[1]) + 15),
+                (0, 0, 255),
+                2,
+            )
             cv.circle(frame, (int(center[0]), int(center[1])), 10, (0, 255, 0), 2)
             trajectory.append((int(center[0]), int(center[1])))
 
         for i in range(1, len(trajectory)):
-            cv.line(frame, trajectory[i-1], trajectory[i], (0, 255, 255), 2)
+            cv.line(frame, trajectory[i - 1], trajectory[i], (0, 255, 255), 2)
 
         out.write(frame)
 
-
     cap.release()
     out.release()
+
 
 if __name__ == "__main__":
     main()
